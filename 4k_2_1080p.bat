d7beb80722ec2380340c4815ea58dbd7a768d38e
Evan Tchelepi <etchelepi+github@gmail.com>
2019-11-26 22:51:43 -0500

    Add files via upload
    Fixed Portrait Mode Issue

--- 4k_2_1080p.bat
+++ 4k_2_1080p.bat
@@ -1,34 +1,26 @@
-REM ###########################################################################
-REM This is a script that I wrote to go through all my video files, get ones 
-REM that are bigger then 1080p and then downscale them to h265 at 1080p
-REM because I want to save space of these old videos from my Phone. 
-REM 4K content on my phone was better, but Honestly, i don't think I need 4K
-REM from these phones. So 4K-> 1080P is superior. 
-
-REM This script does not do anything to the original content. 
-REM You have to erase them yourself if you want.
-REM ###########################################################################
-
-
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
 
-
-REM we only want to do this if the folder doesn't exist
 if NOT EXIST finished (
     mkdir finished
 )
 
-REM I want to report out how many files I found. It helps to know
-set /A COUNTER=0
+::First step: Save the input varariables. Inputfilename should not include the .avi|.mov|.mpg
+::Second Step: Run the mencoder on both the left and right files
+::mencoder format: mencoder -fps <rate> -o <conformed_file> -ovc copy -nosound <source_file>
 
+::convert the temp video a smaller file
+::ffmpeg -y -i %input_name% -c:v prores_ks -profile:v 1  -vendor ap10 -pix_fmt yuv422p10le -s 960x540 %output_name%_proxy.mov
+
+set /A COUNTER=0
 REM use a lower CRF to get better results. 18 is the best
+
 for /r %%i in (*mp4) do (
     Set Name=%%~nxi
     set TEMPPATH=%%~dpnxi
     set FULL_PATH="!TEMPPATH!"
     
-    REM we need to clear it each time. Otherwise we can get some bad behavior.
+    REM we need to clear it each time. A non rotated field returns nothing
     set ROTATE=0
     set /A WIDTH=0
     set /A HEIGHT=0
@@ -55,7 +47,7 @@ for /r %%i in (*mp4) do (
         if !WIDTH! GTR !HEIGHT! (
             if !HEIGHT! GTR 1080 (
                 echo "Portait with Rotation" !WIDTH! !HEIGHT! !ROTATE!
-                ffmpeg -y -i !FULL_PATH! -map_metadata 0 -vf scale=-1:1080 -c:v libx265 -crf 20 -preset slow -c:a copy ./finished/!Name!
+                ffmpeg -y -i !FULL_PATH! -map_metadata 0 -vf scale=1080:-1 -c:v libx265 -crf 20 -preset slow -c:a copy ./finished/!Name!
                 set /A COUNTER+=1
             )
         REM 1080x1920 90 = landscape    
@@ -84,9 +76,6 @@ for /r %%i in (*mp4) do (
         )
     )
 )
-
-REM Note: There are more possibilities. You can have 0,90,180,270 for the rot. None of my content ever had anything but 90 and 0. But it means this script could be improved by
-REM calculating the ROT into a common value in case you run into a video that uses a ROT 270 or 180.
 
 echo "TOTAL NUMBER of 4K Content: " %Counter%
 
